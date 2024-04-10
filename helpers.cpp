3691ce3a5970ee44db2f51dab8cba5162e111699
elenachau <elenaxcha@gmail.com>
2024-04-09 21:20:18 -0700

    delete song from playlist

--- helpers.cpp
+++ helpers.cpp
@@ -97,8 +97,93 @@ bool addSongToPlaylist(Playlist&, const Playlist&){
 
 }
 
-bool deleteSongFromPlaylist(Playlist&){
+bool deleteSongFromPlaylist(Playlist& playlist){
+    int* selections = new int[playlist.getNumSongsLoaded()];
+    int songChoice, count = 0;
+    char cont;
+    int numSongs = playlist.getNumSongsLoaded();
+    cout << "====" << playlist.getName() << "====" << endl;
+    cout << playlist;
 
+    cout << playlist.getNumSongsLoaded() + 1 << ". ";
+    if(playlist.getNumSongsLoaded() + 1 < 10) {
+        cout << "  ";
+    }
+    else if(playlist.getNumSongsLoaded() + 1 < 100) {
+        cout << " ";
+    }
+    cout << "Return to Playlist Options Menu." << endl;
+
+    cout << playlist.getNumSongsLoaded() + 2 << ". ";
+    if(playlist.getNumSongsLoaded() + 2 < 10) {
+        cout << "  ";
+    }
+    else if(playlist.getNumSongsLoaded() + 2 < 100) {
+        cout << " ";
+    }
+    cout << "Exit Program." << endl;
+
+    do{
+        cout << "Select a song: ";
+        getUserEntry(songChoice);
+        bool alreadyDeleted = false;
+        
+        for(int i = 0; i < count; i++){
+            if(songChoice == selections[i]){
+                alreadyDeleted = true;
+            }
+        }
+        if(!alreadyDeleted && songChoice <= numSongs){
+            selections[count] = songChoice;
+            count+=1;
+        }
+    }while(songChoice != numSongs + 1 && songChoice != numSongs + 2 && count != numSongs); //check is count != numSongs because 0-based index
+
+    if(count > 1) {
+        for(int i = 0; i < count; i++){
+            for(int j = 0; j < count - i - 1; j++){
+                if(selections[j] < selections[j+1]){
+                    int temp = selections[j];
+                    selections[j] = selections[j+1];
+                    selections[j+1] = temp;
+                }
+            }
+        }
+
+        //other implementation, not as effective
+        // for(int i = 0; i < count; i++){
+        //     for(int j = 0; j < count; j++){
+        //         if(selections[j] < selections[j+1]){
+        //             int temp = selections[j];
+        //             selections[j] = selections[j+1];
+        //             selections[j+1] = temp;
+        //         }
+        //     }
+        // }
+    }
+
+    for(int i = 0; i < count; i++){
+        playlist - selections[i];
+    }
+    
+    if(playlist.getNumSongsLoaded() == 0){
+        cout << "All songs are deleted. Removing playlist." << endl;
+        string fileToRemove = playlist.getName() + ".txt";
+        remove(fileToRemove.c_str()); //delete playlist; must cast expected parameter of const char* with c_str() to turn obj to C-style str
+        // delete &playlist;
+        cout << "Return to main menu (y/n)?";
+        getUserEntry(cont);
+        if(cont == 'n' || cont == 'N'){
+            return true;
+        }
+        return false;
+    }
+
+    writePlaylistToFile(playlist);
+    if(songChoice == numSongs + 2){
+        return true;
+    }
+    return false;
 }
 
 bool deletePlaylist(Playlist&){
