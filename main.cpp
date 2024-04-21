16d3d080d6c5fee10c5cc97a72e4299956afc0d0
elenachau <elenaxcha@gmail.com>
2024-04-20 18:26:17 -0700

    create main.cpp for playlist menu

--- main.cpp
+++ main.cpp
@@ -1,1 +1,48 @@
 #include "helpers.h"
+
+int main(){
+    Playlist *all = new Playlist;
+    all->setName("dbSmall");
+    ifstream inFile("dbSmall.txt");
+    readData(inFile, *all);
+
+    int userEntry;
+    bool exitProgram;
+
+    do{
+        Playlist* newest = new Playlist;
+        Playlist* selectedPlaylist = new Playlist;
+        system("clear");
+        displayMainMenu();
+        
+        do{
+            cout << "Make a selection" << endl;
+            getUserEntry(userEntry);
+        }while(userEntry < 1 && userEntry > 3);
+
+        system("clear");
+
+        switch(userEntry){
+            case 1:
+                exitProgram = manageExistingPlaylists(*all, *newest);
+                if(exitProgram){
+                    return 0;
+                }
+                break;
+            case 2:
+                exitProgram = createNewPlaylist(*all, *newest);
+                if(exitProgram){
+                    return 0;
+                }
+                break;
+            case 3:
+                exitProgram = true;
+                break;
+            default:
+                cout << "Invalid option. Please try again." << endl;
+                break;
+        }
+    }while(userEntry != 3 && !exitProgram);
+
+    return 0;
+}
